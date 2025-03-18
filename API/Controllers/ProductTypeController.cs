a37f2df62c843c19d32b8d205ba4039d30055437
phanhieunghiaa <phanhieunghia352@gmail.com>
2025-03-18 19:04:19 +0700

    productTypes to product-types

--- API/Controllers/ProductTypeController.cs
+++ API/Controllers/ProductTypeController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/productTypes")]
+    [Route("api/product-types")]
     [ApiController]
     public class ProductTypeController : ControllerBase
     {
