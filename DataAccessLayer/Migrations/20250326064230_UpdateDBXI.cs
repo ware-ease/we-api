20e5f917bf2aec2fc77d1266fde808cdf4db3a67
phanhieunghiaa <phanhieunghia352@gmail.com>
2025-03-26 13:43:14 +0700

    update db

---
+++ DataAccessLayer/Migrations/20250326064230_UpdateDBXI.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDBXI : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Warehouse",
+                type: "decimal(11,8)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,8)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Warehouse",
+                type: "decimal(11,8)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,8)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Warehouse",
+                type: "decimal(10,8)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(11,8)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Warehouse",
+                type: "decimal(10,8)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(11,8)",
+                oldNullable: true);
+        }
+    }
+}
